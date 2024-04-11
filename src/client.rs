b7e9ddff254207549870e1ad3ac6ebfbdf9ba02f
an0nymoos3 <moosely123@gmail.com>
2024-04-12 00:50:42 +0200

    initial rcon built

--- src/client.rs
+++ src/client.rs
@@ -1,9 +1,9 @@
+use crate::message::{Packet, PacketType};
+use anyhow::{anyhow, Result};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::TcpStream,
 };
-
-use crate::message::{AError, AResult, Packet, PacketType};
 
 /// Represents a connection to a server. Makes it easier to send and recieve requests.
 pub struct Connection {
@@ -12,34 +12,39 @@ pub struct Connection {
 
 impl Connection {
     /// Creates new Connection.
-    pub async fn new(ip: &str, port: &str) -> Result<Self, String> {
+    pub async fn new(ip: &str, port: &str) -> Result<Self> {
         match TcpStream::connect(format!("{ip}:{port}")).await {
             Ok(stream) => Ok(Self { conn: stream }),
-            Err(e) => Err(e.to_string()),
+            Err(e) => Err(anyhow!(e)),
         }
     }
 
     /// Establish connection to RCON server.
-    pub async fn connect(&mut self, server_password: &str) -> AResult {
-        let packet: Packet = Packet::new(PacketType::Auth, server_password);
-        self.send(packet).await
+    pub async fn connect(&mut self, server_password: &str) -> Result<()> {
+        let packet: Packet = Packet::new(PacketType::Auth, server_password)?;
+        self.send(packet).await?;
+        Ok(())
     }
 
     /// Disconnect from RCON server.
-    pub async fn disconnect(&mut self) {
-        self.conn.shutdown().await.unwrap();
+    pub async fn disconnect(&mut self) -> Result<()> {
+        self.conn.shutdown().await?;
+        Ok(())
     }
 
     /// Send message to RCON server.
-    pub async fn send(&mut self, message: Packet) -> AResult {
+    pub async fn send(&mut self, message: Packet) -> Result<Packet> {
         let payload = message.encode();
 
-        if let Err(e) = self.conn.write_all(&payload).await {
-            return AResult::Err(AError::TcpError(e.to_string()));
-        }
+        self.conn.write_all(&payload).await?;
 
         let mut response: Vec<u8> = Vec::new();
-        let _ = self.conn.read(&mut response).await;
+
+        // For some reason clippy wants me to explicitly check for error instead of propagating.
+        if let Err(e) = self.conn.read(&mut response).await {
+            return Err(anyhow!(e));
+        }
+
         Packet::decode(response)
     }
 }
--- src/message.rs
+++ src/message.rs
@@ -1,15 +1,7 @@
+use anyhow::{anyhow, Result};
+
 const MAX_SIZE: i32 = 4096;
 const PACKET_PADDING: i32 = 10; // See Valve's documentaion under "Packet Size"
-
-pub enum AResult {
-    Ok(Packet),
-    Err(AError),
-}
-
-pub enum AError {
-    AuthError(String),
-    TcpError(String),
-}
 
 /// Packet types as defined by valve.
 pub enum PacketType {
@@ -19,6 +11,7 @@ pub enum PacketType {
     Execcommand,
 }
 
+/// Rust representation of a packet.
 pub struct Packet {
     size: i32,
     id: i32,
@@ -28,7 +21,7 @@ pub struct Packet {
 
 impl Packet {
     /// Construct a new Packet to send to server.
-    pub fn new(p_type: PacketType, body: &str) -> Self {
+    pub fn new(p_type: PacketType, body: &str) -> Result<Self> {
         let size: i32 = body.len() as i32 + PACKET_PADDING;
         let id = 1;
         let packet_type: i32 = match p_type {
@@ -38,15 +31,15 @@ impl Packet {
         };
 
         if size > MAX_SIZE {
-            panic!("PACKET TOO LARGE!")
+            return Err(anyhow!("Packet size exceeds MAX_SIZE! (4096)"));
         }
 
-        Self {
+        Ok(Self {
             size,
             id,
             packet_type,
             body: body.to_string(),
-        }
+        })
     }
 
     /// Encodes packet to bytes, that RCON expects
@@ -64,13 +57,13 @@ impl Packet {
 
     /// Basically the reverse of encode. Takes raw data in the form of a vec of bytes, converts it
     /// back into a Packet struct.
-    pub fn decode(bytes: Vec<u8>) -> AResult {
-        let size: i32 = i32::from_le_bytes(bytes[0..4].try_into().unwrap());
-        let id: i32 = i32::from_le_bytes(bytes[4..8].try_into().unwrap());
-        let packet_type: i32 = i32::from_le_bytes(bytes[8..12].try_into().unwrap());
-        let body: String = String::from_utf8(bytes[12..].to_vec()).unwrap();
+    pub fn decode(bytes: Vec<u8>) -> Result<Packet> {
+        let size: i32 = i32::from_le_bytes(bytes[0..4].try_into()?);
+        let id: i32 = i32::from_le_bytes(bytes[4..8].try_into()?);
+        let packet_type: i32 = i32::from_le_bytes(bytes[8..12].try_into()?);
+        let body: String = String::from_utf8(bytes[12..].to_vec())?;
 
-        AResult::Ok(Packet {
+        Ok(Packet {
             size,
             id,
             packet_type,
